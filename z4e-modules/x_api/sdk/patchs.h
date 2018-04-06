0c708d79f73a9fb73d488b0322ae9e67da839ce9
Xiaobaibai <824395314@qq.com>
2018-04-06 16:12:02 +0800

    x_api

--- z4e-modules/x_api/sdk/patchs.h
+++
@@ -1,3 +0,0 @@
-void PatchMaxMoney(int maxmoney);
-void PatchMinMoney(int minmoney);
-void PatchKillMoney(int minmoney);
--- z4e-modules/x_api/sdk/utils.h
+++
@@ -1,3 +0,0 @@
-DWORD UTIL_SIGFind(DWORD dwStartAddr, DWORD dwFindLen, char *sig, int len);
-void UTIL_WriteMemory(void *addr, BYTE *value, int size);
-void UTIL_WriteMemoryBYTE(void *addr, BYTE value);
